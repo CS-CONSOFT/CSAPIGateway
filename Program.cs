daf0fa7559dc2016cdbc52e2afe3267bb6ea4e44
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-02-06 17:58:18 -0300

    update

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddSwaggerGen();
 
 builder.Configuration.AddJsonFile("configuration.json", optional: false, reloadOnChange: true);
 
@@ -21,13 +21,17 @@ builder.Services.AddOcelot();
 
 var app = builder.Build();
 
+// Configure Swagger first
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Then configure Ocelot
 await app.UseOcelot();
 
-
+// Configure other middlewares
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
