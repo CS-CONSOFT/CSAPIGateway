10f0e4f5bfd4e609bc6b68ffeb46c81d694a3327
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-02-06 16:45:14 -0300

    update

--- Program.cs
+++ Program.cs
@@ -21,11 +21,6 @@ builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 await app.UseOcelot();
 
 
