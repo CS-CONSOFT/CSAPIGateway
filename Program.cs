dd7152a10f26fb8df77382403b68f68247e30ef3
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-02-07 10:05:51 -0300

    atualizando para ter swagger

--- Program.cs
+++ Program.cs
@@ -1,44 +1,41 @@
+using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
-
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
-
-builder.Services.AddHttpClient("OcelotHttpClient")
-    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-    {
-        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-    });
-
-
-
 builder.Services.AddOcelot();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "RRBank Gateway API",
+        Version = "v1",
+    });
+});
+
+
+//builder.Services.AddHttpClient("OcelotHttpClient")
+//    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+//    {
+//        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+//    });
+
+
+
 
 //builder.WebHost.UseUrls(["http://0.0.0.0:80"]);
 
 var app = builder.Build();
 
-// Configure Swagger first
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+});
 
-
-
-// Configure other middlewares
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
 // Then configure Ocelot
 await app.UseOcelot();
 app.Run();
