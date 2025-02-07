10fc719f085f656b4c53d183968dae9a153f201e
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-02-07 10:41:52 -0300

    VERSAO_FUNCIONL_SEM_SWAGGER

--- Program.cs
+++ Program.cs
@@ -1,40 +1,18 @@
-using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
+
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
+
+
 builder.Services.AddOcelot();
-builder.Services.AddSwaggerForOcelot(builder.Configuration);
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "RRBank Gateway API",
-        Version = "v1",
-    });
-});
 
-
-//builder.Services.AddHttpClient("OcelotHttpClient")
-//    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-//    {
-//        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-//    });
-
-
-
-
-builder.WebHost.UseUrls(["http://0.0.0.0:80"]);
+//builder.WebHost.UseUrls(["http://0.0.0.0:80"]);
 
 var app = builder.Build();
-
-app.UseSwaggerForOcelotUI(options =>
-{
-    options.PathToSwaggerGenerator = "/swagger/docs";
-});
 
 // Then configure Ocelot
 await app.UseOcelot();
