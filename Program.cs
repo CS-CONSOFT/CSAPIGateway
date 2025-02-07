9ec0d1c08764a00c576ccf0ec88121b7c6e35650
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-02-07 15:29:14 -0300

    update

--- Program.cs
+++ Program.cs
@@ -3,18 +3,37 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
 
-
 builder.Services.AddOcelot();
-//
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); // Isso é importante para o Swagger funcionar
+
+
+
 builder.WebHost.UseUrls(["http://0.0.0.0:80"]);
 
 var app = builder.Build();
 
-// Then configure Ocelot
-await app.UseOcelot();
-app.Run();
+app.UseSwagger(); // Primeiro, habilite o Swagger
 
+
+//app.Map("/swagger/v1/swagger.json", b =>
+//{
+//    b.Run(async x =>
+//    {
+//        var json = File.ReadAllText("swagger.json");
+//        await x.Response.WriteAsync(json);
+//    });
+//});
+
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
+
+
+await app.UseOcelot(); // Depois, execute o Ocelot
+
+app.Run();
