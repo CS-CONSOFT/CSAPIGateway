4b8703b231d6a8fd254130ef0f3f42bed10545e3
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-02-13 09:44:29 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -9,7 +11,28 @@ builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}
 builder.Services.AddOcelot();
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); // Isso é importante para o Swagger funcionar
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API CSBS101"
+    });
+    c.TagActionsBy(api =>
+    {
+        if (api.GroupName != null)
+        {
+            return [api.GroupName];
+        }
+
+        if (api.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+        {
+            return [controllerActionDescriptor.ControllerName];
+        }
+
+        throw new InvalidOperationException("Unable to determine tag for endpoint.");
+    });
+    c.DocInclusionPredicate((name, api) => true);
+});
 
 
 
@@ -31,7 +54,40 @@ builder.WebHost.UseUrls(["http://0.0.0.0:80"]);
 
 var app = builder.Build();
 
-app.UseSwagger(); // Primeiro, habilite o Swagger
+app.UseSwagger(options =>
+{
+    options.PreSerializeFilters.Add((swagger, httpReq) =>
+    {
+        swagger.Servers = new List<OpenApiServer>();
+        if (app.Environment.IsDevelopment())
+        {
+            swagger.Servers.Add(new OpenApiServer
+            {
+                Url = $"{httpReq.Scheme}://{httpReq.Host.Value}",
+                Description = "DEV",
+            });
+            swagger.Servers.Add(new OpenApiServer
+            {
+                Url = "http://api-uat.com",
+                Description = "UAT",
+            });
+        }
+        if (app.Environment.IsProduction())
+        {
+            swagger.Servers.Add(new OpenApiServer
+            {
+                Url = $"{httpReq.Scheme}://{httpReq.Host.Value}",
+                Description = "Production",
+            });
+            swagger.Servers.Add(new OpenApiServer
+            {
+                Url = "http://api-uat.com",
+                Description = "UAT",
+            });
+        }
+    });
+});
+
 
 app.UseCors();
 app.UseSwaggerForOcelotUI(opt =>
